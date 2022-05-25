f0d93a85d168383ce2ee3237cd6eff372b75ecec
Jonas Fredriksson <jonas.fredriksson94@gmail.com>
2022-05-25 20:29:34 +0200

    Allow all origins for sockjs

--- src/main/java/com/example/piwater/userconfig/UserConfiguration.java
+++ src/main/java/com/example/piwater/userconfig/UserConfiguration.java
@@ -1,7 +1,6 @@
 package com.example.piwater.userconfig;
 
 import com.example.piwater.constants.Keys;
-import com.example.piwater.model.Sensor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
--- src/main/java/com/example/piwater/websocket/WebSocketConfig.java
+++ src/main/java/com/example/piwater/websocket/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://10.148.254.53:3000")
+        registry.addEndpoint("/ws").setAllowedOrigins("*")
                 .withSockJS();
 
       /*  registry.addEndpoint("/ws")
