7fb93c201020e2e917a053eb60c1028434b71d66
Jonas Fredriksson <jonas.fredriksson94@gmail.com>
2022-07-01 22:51:46 +0200

    Rework sensor polling scheduling

--- src/main/java/com/example/piwater/scheduling/PollSensors.java
+++ src/main/java/com/example/piwater/scheduling/PollSensors.java
@@ -1,27 +1,26 @@
 package com.example.piwater.scheduling;
 
-import com.example.piwater.constants.Keys;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.TaskScheduler;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import javax.annotation.Resource;
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
-@Component
-public class PollSensors {
+@Configuration
+@EnableScheduling
+public class PollSensors implements SchedulingConfigurer {
     private static final Logger log = LoggerFactory.getLogger(PollSensors.class);
     private final JmsTemplate jmsTemplate;
     private final TaskScheduler executor;
@@ -35,14 +34,31 @@ public class PollSensors {
         this.executor = taskExecutor;
     }
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void onSchedule() {
-        schedulePolling(new PollSensorsTask(userSettings, jmsTemplate));
-    }
-    public void schedulePolling(final Runnable task) {
-        log.info("Sensor polling will be scheduled");
-        executor.scheduleWithFixedDelay(task, (int) userSettings.getOrDefault("sensorCheckTime", 120000)
-);
+    @Bean
+    public Executor taskExecutor() {
+        return Executors.newScheduledThreadPool(100);
     }
 
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setScheduler(taskExecutor());
+        taskRegistrar.addTriggerTask(
+                () -> {
+                    //String sensorTopic = (String) userSettings.getOrDefault(Keys.SENSOR_TOPIC, "topic1");
+                    String sensorTopic = "topic";
+                    log.info("Polling ESP32 to get current state of sensors on topic: {}", sensorTopic);
+                    jmsTemplate.send(sensorTopic, session -> session.createTextMessage("on"));
+                },
+                triggerContext -> {
+                    Calendar nextExecutionTime = new GregorianCalendar();
+                    Date lastActualExecutionTime = triggerContext.lastActualExecutionTime();
+                    nextExecutionTime.setTime(lastActualExecutionTime != null ? lastActualExecutionTime : new Date());
+                    // nextExecutionTime.add(Calendar.MILLISECOND, env.getProperty("myRate", Integer.class)); //you can get the value from wherever you want
+                    nextExecutionTime.add(Calendar.MILLISECOND, 120000); //you can get the value from wherever you want
+                    //TODO: Fix this dynamically
+
+                    return nextExecutionTime.getTime();
+                }
+        );
+    }
 }
