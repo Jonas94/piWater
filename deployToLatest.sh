9b2fbfe574577e4e23138de1a653a0fa6fac048d
Jonas Fredriksson <jonas.fredriksson94@gmail.com>
2022-07-15 21:21:01 +0200

    Fix issue with jar build

--- deployToLatest.sh
+++ deployToLatest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mvn clean install
-scp target/piWater-0.0.1-SNAPSHOT.jar pi@192.168.1.114:~/piWater/piWater-LATEST.jar
+gradle clean build
+scp build/libs/piWater-0.0.1-SNAPSHOT.jar pi@192.168.1.114:~/piWater/piWater-LATEST.jar
